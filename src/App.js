e39fb893b9139e6a1b250ceea8bad51b4fb4cf80
Ritvick Pandey <ritvick.pandey@bfhl.in>
2022-03-05 20:20:20 +0530

    Create grid

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import { Navbar } from "./shared/components/index";
 import Auth from "./shared/auth";
 import Landing from "./screens/landingPage";
+import NewUser from "./screens/newUser";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/new-user" element={<NewUser />} />
           <Route path="/login" element={<Auth />} />
           <Route path="/" element={<Landing />} />
         </Routes>
