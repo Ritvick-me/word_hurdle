066dc743beef50c90471aab66cd9868d6cc9e861
Rishav-Chowdhury <rishavthegenius@gmail.com>
2022-03-05 12:40:59 +0530

    Added Leaderboard Folder

---
+++ src/screens/leaderboardPage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+const leaderboardPage = () => {
+  return <div className="leaderboard">Leader Board</div>;
+};
+
+export default leaderboardPage;
