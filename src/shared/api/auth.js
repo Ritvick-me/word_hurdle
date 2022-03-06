2f6cc8f324b47bde7fbc18c2441e2146d8b6f7f4
Ritvick Pandey <ritvick.pandey@bfhl.in>
2022-03-06 15:51:58 +0530

    API fixes

--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -3,8 +3,13 @@ import { http } from "./http";
 
 const backend_base_URL = process.env.REACT_APP_BACKEND_URL;
 
-export const getUser = async () => {
-  const res = await http.get(`${backend_base_URL}/auth/user`);
+export const getUser = async (userExists) => {
+  let res = null;
+  if (userExists) {
+    res = await http.get(`${backend_base_URL}/auth/user`);
+  }
+  res = await http.get(`${backend_base_URL}/auth/user/firsttime`);
+  console.log(res);
   return res.data;
 };
 
@@ -17,7 +22,8 @@ export const fetchAccessToken = async (code) => {
   );
   const token = res.data;
   if (token) {
-    console.log("Token: ");
+    console.log("Token: ", token);
   }
+
   return token;
 };
--- src/shared/api/http.js
+++ src/shared/api/http.js
@@ -11,11 +11,14 @@ http.interceptors.request.use(
   async function (recdConfig) {
     const config = recdConfig;
     const access_token = localStorage.getItem("access_token");
+    console.log("access token stored in frontend : ", access_token);
     try {
       const token = localStorage.getItem("token");
+      console.log("remaining token info is ", token);
 
       if (access_token) {
         const Tokens = JSON.parse(token);
+        console.log(Tokens);
         if (Tokens) {
           const { refresh_token, provider, expires_in } = Tokens;
           if (refresh_token && new Date().getTime() + 5 * 6000 > expires_in) {
@@ -32,7 +35,7 @@ http.interceptors.request.use(
               expires_in: new Date().getTime() + 1000 * 30 * 7,
               refresh_token,
             };
-            localStorage.setItem("access_token", newUserToken.accessToken);
+            localStorage.setItem("access_token", newUserToken.access_token);
             localStorage.setItem("token", JSON.stringify(newTokenObj));
           }
         }
